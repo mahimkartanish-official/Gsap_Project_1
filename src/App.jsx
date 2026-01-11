60b87919ca7efb8d372dcd00545868a13e15e8b4
Tanish <mahimkartanish47@gmail.com>
2026-01-11 11:45:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { ScrollTrigger,SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const App = () => {
+    return(
+        <div className="flex-center h-[100vh]">
+            <h1 className="text-xl text-red-300">Tanish Mahimkar</h1>
+        </div>
+    )
+}
+
+export default App
